9662aa56b6cc10ba1518398e039940fd73996677
Michał Pałka <prolativus@gmail.com>
2023-10-19 10:19:11 +0200

    Use scala 3.3.1 and scala-cli 1.0.4

--- project.scala
+++ project.scala
@@ -1,4 +1,4 @@
-//> using scala "3.3.0"
+//> using scala "3.3.1"
 //> using dep "org.apache.spark:spark-core_2.13:3.2.0"
 //> using dep "org.apache.spark:spark-sql_2.13:3.2.0"
 //> using test.dep "org.scalatest::scalatest::3.2.16"

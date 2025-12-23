69bda003cde2e729342c03614acb4d2b8e87f0d3
Michał Pałka <prolativus@gmail.com>
2025-12-23 19:37:21 +0100

    Bump scala to LTS 3.3.7; Bump scala-cli to 1.11.0; Run CI for scala Next 3.7.4; Run with java 11

--- project.scala
+++ project.scala
@@ -1,4 +1,5 @@
-//> using scala 3.3.6
+//> using scala 3.3.7
 //> using dep org.apache.spark:spark-core_2.13:3.2.0
 //> using dep org.apache.spark:spark-sql_2.13:3.2.0
 //> using test.dep org.scalatest::scalatest::3.2.19
+//> using jvm temurin:11
